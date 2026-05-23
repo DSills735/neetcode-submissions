e4d9ed66b08f579c0daa28fd6903c22b42c6b5c0
David Sills <131375261+DSills735@users.noreply.github.com>
2026-05-23 07:59:00 -0500

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        int start = 0;
+        string trimmed = s.Trim().ToLower();
+        int end = trimmed.Length - 1;
+
+        while(start < end ){
+            if(!char.IsLetterOrDigit(trimmed[end])){
+                end -= 1;
+                continue;
+            }
+            else if(!char.IsLetterOrDigit(trimmed[start])){
+                start += 1;
+                continue;
+            }
+            else if(trimmed[start] != trimmed[end]){
+                return false;
+            }
+            else{
+                start += 1;
+                end -= 1;
+            }
+        }
+        return true;
+    }
+}
